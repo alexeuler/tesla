d6704451770d2825d6bf8a8399b89efcebfd93ab
alex@laptop <karasev.alexey@gmail.com>
2013-11-16 00:32:55 +0400

    added some styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
+# -*- coding: utf-8 -*-
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :set_view_params
+
+  protected
+
+  def set_view_params
+    @crumbs={'Главная'=>root_path}
+  end
+
+
 end
