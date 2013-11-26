eeb80f1423d112899c21c2f152d7b5e22b295c1f
Alexey Karasev <alex@homa-online.ru>
2013-11-26 07:05:01 +0000

    added image tag with no digest helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def image_tag_zen(link, options={})
+    cls=options[:class]||""
+    alt=options[:alt]||""
+    "<img src=\'assets/images/#{link}\' class=\'#{cls}\' alt=\'#{alt}\'>".html_safe
+  end
 end
