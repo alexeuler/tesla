82b8c365e178a31c57e972d627c7bc8ff29e4d40
Alexey Karasev <alex@homa-online.ru>
2013-11-26 07:17:14 +0000

    moved images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@ module ApplicationHelper
   def image_tag_zen(link, options={})
     cls=options[:class]||""
     alt=options[:alt]||""
-    "<img src=\'assets/images/#{link}\' class=\'#{cls}\' alt=\'#{alt}\'>".html_safe
+    "<img src=\'images/#{link}\' class=\'#{cls}\' alt=\'#{alt}\'>".html_safe
   end
 end
